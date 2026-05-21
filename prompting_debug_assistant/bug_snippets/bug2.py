0edd017141bc7a5b1b27dc45c73f20033ba78a5b
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-21 14:52:41 +0400

    000

--- prompting_debug_assistant/bug_snippets/bug2.py
+++
@@ -1,8 +0,0 @@
-def divide_numbers(a, b):
-    # Divide a by b
-    return a / b  # BUG: division by zero not handled
-
-# Test cases
-print(divide_numbers(10, 2))
-print(divide_numbers(10, 0))  # triggers exception
-print(divide_numbers(5, 5))
