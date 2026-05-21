3e68960e68487a59afdfc398ab58bed863c3ec1f
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-21 14:35:40 +0400

    0 0 2

--- prompting_debug_assistant/bug_snippets/bug6.java
+++ prompting_debug_assistant/bug_snippets/bug6.java
@@ -1,6 +1,13 @@
-public class Bug6 { 
-    public static void main(String[] args) { 
-            System.out.println("Count: " + i); 
-        } 
-    } 
-} 
+public class Bug6 {
+    public static void main(String[] args) {
+        // Print numbers 0 to 4
+        for (int i = 0; i <= 5; i++) { // BUG: should be i < 5
+            System.out.println("Count: " + i);
+        }
+
+        // Another loop example
+        for (int j = 0; j < 3; j++) {
+            System.out.println("Secondary count: " + j);
+        }
+    }
+}
